54df61d3d89202af485c198835427009a87a129a
Starshot Software <starshotsoftware@Starshot-203.local>
2017-11-17 12:36:17 +0100

    City Implementation

---
+++ Challenge1_2/Helpers/CityHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Challenge1_2.Models;
+using Newtonsoft.Json;
+
+namespace Challenge1_2.Helpers
+{
+    public class CityHelper
+    {
+
+        const string API = "maps.googleapis.com/maps/api/place/autocomplete/json?input=";
+        const string APIKEY = "&types=(cities)&key=AIzaSyDQ0Zzrac5HUEw0_j0O8XaVW9_e-EDGVag";
+
+        public static async Task<CityInformation> GetCurrentCity (string city)
+        {
+            string url = $"http://{API}{city}{APIKEY}";
+
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(url);
+            var result = JsonConvert.DeserializeObject<WeatherInformation.ResponseWeather>(response);
+            return result;
+        }
+    }
+}
---
+++ Challenge1_2/Models/CityInformation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Challenge1_2.Common;
+
+namespace Challenge1_2.Models
+{
+    public class CityInformation : ObservableBase
+    {
+
+        List<Predictions> _predictions;
+        public List<Predictions> predictions
+        {
+            get { return _predictions; }
+            set { SetProperty(ref _predictions, value); }
+        }
+
+        public class Predictions : ObservableBase
+        {
+            
+            Structured_formatting _structured_formatting;
+            public Structured_formatting structured_formatting
+            {
+                get { return _structured_formatting; }
+                set { SetProperty(ref _structured_formatting, value); }
+            }
+
+        }
+
+        public class Structured_formatting : ObservableBase
+        {
+
+            String _main_text;
+            public string main_text
+            {
+                get { return _main_text; }
+                set { SetProperty(ref _main_text, value); }
+            }
+
+            String _secondary_text;
+            public string secondary_text
+            {
+                get { return _secondary_text; }
+                set { SetProperty(ref _secondary_text, value); }
+            }
+
+        }
+
+    }
+}
